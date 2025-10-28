ad61f0c4e5f6a485d249ac4d714c0a86d966d27e
mkabg <m7110645k@gmail.com>
2025-10-28 17:29:24 +0200

    fix

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -12,7 +12,7 @@ import { AlertContext } from "./context/AlertOnContext";
 import { alertOnApi } from "./api";
 import ChangePassword from "./Pages/changePassword/ChangePassword";
 
-export const URL = "https://nachsal-emergency.onrender.com";
+export const URL = "https://nachsal-emergency-fdsj.onrender.com";
 
 export default function App() {
   const [soldier, setSoldier] = useState<Soldier | null>(null);
